477178d5734624cc50cce203adb429d52e064068
Matthew Guzman <tomaffew@me.com>
2019-07-04 20:50:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,15 +12,21 @@ def monopoly_with_second_tier
 	    pieces: 4,
 	    :names=>{},
 	    :rent_in_dollars=>{}
-	      
-	   
 	  }
-	}
+	 }
 
 end
 
 def monopoly_with_third_tier
-	
+		monopoly = {
+	  :railroads=> {
+	    pieces: 4,
+	    :names=>{},
+	    :rent_in_dollars=>{
+	      :rent_in_dollars=>25
+	    }
+	  }
+	 }
 
 end
 
